c4d3c1a1e6080c2f4b0624a0b567d6aee8a1c6c8
dengjialong <dengjialong@datahunter.cn>
2018-01-24 17:34:18 +0800

    去除自动建表

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-
 	_ "admin.dh.cn/routers"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
-
 )
 
 func main() {
-
-
-	// 自动建表
-	orm.RunSyncdb("default", false, true)
 	beego.Run()
 }
-
