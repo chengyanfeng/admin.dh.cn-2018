68b9c78fe6bc58a01af64ed08677eb6cdc2f5223
chengyanfeng <569498977@qq.com>
2018-01-25 20:43:15 +0800

    登陆bug 已经解决！！！是返回码的问题。用cookie 和session 现在是没有区别的

--- filter/basefilter.go
+++ filter/basefilter.go
@@ -2,21 +2,15 @@ package filter
 import (
 	"common.dh.cn/utils"
 	"github.com/astaxie/beego/context"
-	"fmt"
 )
-
 var BaseFilter = func(ctx *context.Context) {
-	fmt.Println(ctx.Request.RequestURI,"------------url---------")
 	if ctx.Request.RequestURI  !="/"  {
 		if ctx.Request.RequestURI  !="/login" {
 			if ctx.Request.RequestURI !="/login/quit" {
 				gooid:= ctx.Input.Session("gooid")
 			if len(utils.ToStrings(gooid))>0 {
 					cachgooid:=utils.S("gooid")
-					fmt.Println(gooid,"---------------gooid-----------------")
-					fmt.Println(cachgooid,"---------------gooid-----------------")
 					if cachgooid!=gooid {
-						fmt.Println(cachgooid,"---------------跳回去了-----------------")
 						ctx.Redirect(302,"/")
 					}
 
@@ -26,7 +20,6 @@ var BaseFilter = func(ctx *context.Context) {
 			}
 		}
 
-		fmt.Println("------------------------login----------------")
 	}
 }
 
