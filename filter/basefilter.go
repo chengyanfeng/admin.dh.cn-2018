aea67d01f8fd0cd18433af9868f3a71bef5bd4e9
dengjialong <dengjialong@datahunter.cn>
2018-01-26 15:36:57 +0800

    过滤器

--- filter/basefilter.go
+++ filter/basefilter.go
@@ -1,39 +1,14 @@
 package filter
+
 import (
 	"common.dh.cn/utils"
 	"github.com/astaxie/beego/context"
 )
+
 var BaseFilter = func(ctx *context.Context) {
-	if ctx.Request.RequestURI  !="/"  {
-		if ctx.Request.RequestURI  !="/login" {
-			if ctx.Request.RequestURI !="/login/quit" {
-				gooid:= ctx.Input.Session("gooid")
-			if len(utils.ToStrings(gooid))>0 {
-					cachgooid:=utils.S("gooid")
-					if cachgooid!=gooid {
-						ctx.Redirect(302,"/")
-					}
-
-				}else {
-					ctx.Redirect(302,"/")
-				}
-			}
-		}
-
+	gooid := ctx.Input.Session("gooid")
+	cachgooid := utils.S("gooid")
+	if cachgooid != gooid {
+		ctx.Redirect(302, "/login")
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- routers/router.go
+++ routers/router.go
@@ -1,34 +1,41 @@
 package routers
 
 import (
-	. "admin.dh.cn/filter"
 	"admin.dh.cn/controllers"
+	. "admin.dh.cn/filter"
 	"github.com/astaxie/beego"
-
 )
 
 func init() {
 
-	beego.InsertFilter("/*", beego.BeforeRouter, BaseFilter)
-	beego.Router("/", &controllers.LoginController{},"get:Get")
-	beego.Router("/login", &controllers.LoginController{},"post:Login")
-	beego.Router("/login/quit/",&controllers.LoginController{},"get:Quit")
-	beego.Router("/index", &controllers.IndexController{},"get:Get")
-	beego.Router("/user", &controllers.UserController{},"get:List")
-	beego.Router("/user/getuserdata", &controllers.UserController{},"get:GetUserData")
-	beego.Router("/user/create", &controllers.UserController{},"get:Create")
-	beego.Router("/user/edit", &controllers.UserController{},"get:Edit")
-	beego.Router("/user/add", &controllers.UserController{},"post:Add")
-	beego.Router("/user/update", &controllers.UserController{},"post:Update")
-	beego.Router("/user/remove", &controllers.UserController{},"get:Remove")
-	beego.Router("/user/updateStatusAva", &controllers.UserController{},"get:UpdateStatusAva")
-	beego.Router("/user/getCorp", &controllers.UserController{},"get:GetCorp")
-	beego.Router("/user/delectAndAddCorp", &controllers.UserController{},"get:DelectAndAddCorp")
-	beego.Router("/user/listremove", &controllers.UserController{},"post:Listremove")
-	beego.Router("/user/listChangeType", &controllers.UserController{},"post:ListChangeType")
-	beego.Router("/user/changeType", &controllers.UserController{},"get:ChangeType")
-	beego.Router("/user/getUserScreen", &controllers.UserController{},"get:GetUserScreen")
-	beego.Router("/user/delectUserScreen", &controllers.UserController{},"get:DelectUserScreen")
+	beego.Router("/login", &controllers.LoginController{}, "get:Get")
+	beego.Router("/login", &controllers.LoginController{}, "post:Login")
+	beego.Router("/login/quit/", &controllers.LoginController{}, "get:Quit")
+	beego.Router("/", &controllers.IndexController{}, "get:Get")
+
+	beego.InsertFilter("/user/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/corp/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/sourcetype/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/source/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/screen/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/userscreen/*", beego.BeforeRouter, BaseFilter)
+	beego.InsertFilter("/invitationcode/*", beego.BeforeRouter, BaseFilter)
+
+	beego.Router("/user", &controllers.UserController{}, "get:List")
+	beego.Router("/user/getuserdata", &controllers.UserController{}, "get:GetUserData")
+	beego.Router("/user/create", &controllers.UserController{}, "get:Create")
+	beego.Router("/user/edit", &controllers.UserController{}, "get:Edit")
+	beego.Router("/user/add", &controllers.UserController{}, "post:Add")
+	beego.Router("/user/update", &controllers.UserController{}, "post:Update")
+	beego.Router("/user/remove", &controllers.UserController{}, "get:Remove")
+	beego.Router("/user/updateStatusAva", &controllers.UserController{}, "get:UpdateStatusAva")
+	beego.Router("/user/getCorp", &controllers.UserController{}, "get:GetCorp")
+	beego.Router("/user/delectAndAddCorp", &controllers.UserController{}, "get:DelectAndAddCorp")
+	beego.Router("/user/listremove", &controllers.UserController{}, "post:Listremove")
+	beego.Router("/user/listChangeType", &controllers.UserController{}, "post:ListChangeType")
+	beego.Router("/user/changeType", &controllers.UserController{}, "get:ChangeType")
+	beego.Router("/user/getUserScreen", &controllers.UserController{}, "get:GetUserScreen")
+	beego.Router("/user/delectUserScreen", &controllers.UserController{}, "get:DelectUserScreen")
 
 	//自动注入路由
 	beego.AutoRouter(&controllers.CorpController{})
@@ -37,9 +44,5 @@ func init() {
 	beego.AutoRouter(&controllers.UserscreenController{})
 	beego.AutoRouter(&controllers.InvitationCodeController{})
 	beego.AutoRouter(&controllers.SourceController{})
-
-
-
-
 
 }
