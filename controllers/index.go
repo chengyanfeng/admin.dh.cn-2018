f310c0c89a037d23c1adc1b4d90045f4dbb8be57
dengjialong <dengjialong@datahunter.cn>
2018-02-02 14:24:32 +0800

    fix

---
+++ controllers/index.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"fmt"
+
+	"common.dh.cn/def"
+	"common.dh.cn/utils"
+)
+
+type IndexController struct {
+	AdminController
+}
+
+func (c *IndexController) Get() {
+	c.init(0)
+	c.TplName = "index/index.html"
+}
+
+func (c *IndexController) FileUploader() {
+	data := c.GetString("data")
+	bin := utils.Base64Decode(data)
+	name := c.GetString("name")
+	index, _ := c.GetInt64("index")
+	size, _ := c.GetInt64("size")
+	total, _ := c.GetInt64("total")
+	ext := utils.ToString(utils.Pathinfo(name)["extension"])
+	if size > def.MAX_UPLOAD_SIZE {
+		c.EchoJsonErr("文件尺寸不得大于", def.MAX_UPLOAD_SIZE)
+	}
+	md5 := utils.Md5(name)
+	file_path := fmt.Sprintf("upload/%v.%v", md5, ext)
+	utils.WriteFile(file_path, bin)
+	real_url := c.Hostname() + "/" + file_path
+	result := utils.P{
+		"result":     1,
+		"index":      index,
+		"total":      total,
+		"name":       "name",
+		"real_url":   real_url,
+		"file_index": c.GetString("file_index"),
+	}
+	c.EchoJson(result)
+}
